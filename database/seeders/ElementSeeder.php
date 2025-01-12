8e407c9e0beabf58024eceb21e6297b1abffac64
MatteoDiMario96 <dm.matteo96@live.it>
2025-01-12 20:50:52 +0100

    Seedato nel db con le fillable.

--- database/seeders/ElementSeeder.php
+++ database/seeders/ElementSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Element;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,42 @@ class ElementSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $elementsData = [
+            [
+                'name' => 'Colorless',
+                'IMG' => 'Element_Colorless.jpg',
+            ],[
+                'name' => 'Dark',
+                'IMG' => 'Element_Dark.jpg',
+            ],[
+                'name' => 'Dragon',
+                'IMG' => 'Element_Dragon.png',
+            ],[
+                'name' => 'Electric',
+                'IMG' => 'Element_Electric.png',
+            ],[
+                'name' => 'Fighting',
+                'IMG' => 'Element_Fighting.png',
+            ],[
+                'name' => 'Fire',
+                'IMG' => 'Element_Fire.jpg',
+            ],[
+                'name' => 'Grass',
+                'IMG' => 'Element_Grass.png',
+            ],[
+                'name' => 'Psychic',
+                'IMG' => 'Element_Psychic.webp',
+            ],[
+                'name' => 'Steel',
+                'IMG' => 'Element_Steel.png',
+            ],[
+                'name' => 'Water',
+                'IMG' => 'Element_Water.png',
+            ],
+        ];
+
+        foreach ($elementsData as $elementData ) {
+            Element::create($elementData);
+        }
     }
 }
